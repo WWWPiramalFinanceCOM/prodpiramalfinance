871c5d440b1257fd7f29ffaf832eccf34b8dcb66
pashtepooja18 <poojapashte18@gmail.com>
2025-09-02 19:50:54 +0530

    api code changes

--- blocks/applyloanform/statemasterapi.js
+++ blocks/applyloanform/statemasterapi.js
@@ -44,13 +44,6 @@ export function statemasterGetStatesApi(loanType) {
   const url = allowedtype ? fetchUrl : graphqlUrl;
 
   return new Promise((resolve, reject) => {
-    // const url = '/graphql/execute.json/piramalfinance/State%20City%20Master';
-
-    let url = '/graphql/execute.json/piramalfinance/State%20City%20Master';
-    if(window.location.href.includes('localhost')){
-      url = 'https://www.piramalfinance.com/graphql/execute.json/piramalfinance/State%20City%20Master';
-    }
-    // let stateMasterGraphQLQuery = "query MyQuery { statemasterList { items { state, data } } }";
     
     fetchAPI('GET', url)
       .then(async (response) => {
